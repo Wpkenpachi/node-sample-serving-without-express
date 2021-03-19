bc1755c10653721e1c52e463b98b9f5b9ac793af
Wesley <angelportinari17@gmail.com>
2021-03-19 18:51:28 -0300

    Uncommited files

---
+++ src/api/name_list.js
@@ -0,0 +1,14 @@
+// mocked data
+const lista = [
+  "nostrud",
+  "aliquip",
+  "laboris",
+  "pariatur",
+  "esse"
+]
+
+function getList() {
+  return JSON.stringify(lista)
+}
+
+module.exports = getList
