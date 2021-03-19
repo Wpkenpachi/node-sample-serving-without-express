009b56012b31bbf77263997b72e69e11b9b34d5b
Wesley <angelportinari17@gmail.com>
2021-03-19 18:39:00 -0300

    Added a request example

--- src/app.js
+++ src/app.js
@@ -5,9 +5,23 @@ const serveStatic = require('serve-static')
 // Serve up public/ftp folder
 const serve = serveStatic('src/static', { 'index': ['index.html'] })
 
+// mocked data
+const lista = [
+  "nostrud",
+  "aliquip",
+  "laboris",
+  "pariatur",
+  "esse"
+]
+
 // Create server
 const server = http.createServer(function onRequest(req, res) {
-  serve(req, res, finalhandler(req, res))
+  if (req.method == 'GET' && req.url == '/list') {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(lista))
+  } else {
+    serve(req, res, finalhandler(req, res))
+  }
 })
 
 // Listen
--- src/static/js/pagina-cardapio.js
+++ src/static/js/pagina-cardapio.js
@@ -1,3 +1,20 @@
-function show(params) {
-  console.log('Show!')
+const instance = axios.create({
+  baseURL: `http://localhost:3000/`,
+});
+
+
+function getList() {
+  instance.get('list').then(response => {
+    const { data, status } = response;
+    if (status == 200) {
+      data.map(item => {
+        const li = document.createElement('li')
+        li.textContent = item
+        document.getElementById('lista').appendChild(li)
+      })
+    } else {
+      console.log(data)
+    }
+
+  })
 }
